3d28b4d6188f98d01136a54bd341cc6b8346bb9d
George <george@evbettor.com>
2022-09-27 00:41:11 +0000

    added new functions

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ embed = discord.Embed()
 def get_dat(num_1, num_3):
     query = {'a': num_1, 'b': num_3}
     response = requests.get(
-        'http://ec2-3-139-73-197.us-east-2.compute.amazonaws.com:8000/sum',
+        'http://ec2-18-222-189-197.us-east-2.compute.amazonaws.com:8000/sum',
         params=query)
     return response.json()
 
@@ -37,9 +37,9 @@ async def on_message(message):
 
     msg = message.content
 
-    if message.content.startswith('$yo'):
+    if message.content.startswith('gm'):
         await message.channel.send(
-            'http://ec2-3-139-73-197.us-east-2.compute.amazonaws.com:8000/plot_player?player_name=Miles%20Sanders'
+            'GM'
         )
         #dat = get_dat(msg)
         #await message.channel.send(dat)
@@ -49,14 +49,14 @@ async def on_message(message):
 @bot.command()
 async def plot(ctx, player):
     await ctx.send(
-        'http://ec2-3-139-73-197.us-east-2.compute.amazonaws.com:8000/plot_player?player_name='
+        'http://ec2-18-222-189-197.us-east-2.compute.amazonaws.com:8000/plot_player?player_name='
         + urllib.parse.quote(player))
 
 
 @bot.command()
 async def floor(ctx, player):
     response = requests.get(
-        'http://ec2-3-139-73-197.us-east-2.compute.amazonaws.com:8000/floor?player_name='
+        'http://ec2-18-222-189-197.us-east-2.compute.amazonaws.com:8000/floor?player_name='
         + urllib.parse.quote(player)).json()
     str_response = json.dumps(response, indent=2)
     await ctx.send('```\n' + str_response + '\n```')
@@ -64,12 +64,43 @@ async def floor(ctx, player):
 
 @bot.command()
 async def xray(ctx, user):
-    link = 'http://ec2-3-139-73-197.us-east-2.compute.amazonaws.com:8000/xray?user=' + urllib.parse.quote(
+    link = 'http://ec2-18-222-189-197.us-east-2.compute.amazonaws.com:8000/xray?user=' + urllib.parse.quote(
         user)
-    embed.description = user + " [dowmload](" + link + ")."
+    embed.description = user + " [download](" + link + ")."
     await ctx.send(embed=embed)
-    #  await ctx.send('http://ec2-3-139-73-197.us-east-2.compute.amazonaws.com:8000/xray?user='
+    #  await ctx.send('http://ec2-18-222-189-197.us-east-2.compute.amazonaws.com:8000/xray?user='
     #     + urllib.parse.quote(user))
 
+  
+@bot.command()
+async def value(ctx, user):
+    statement = requests.get('http://ec2-18-222-189-197.us-east-2.compute.amazonaws.com:8000/portfolio?user='
+        + urllib.parse.quote(user))
+    statement = statement.json()[0]
+    await ctx.send(
+        statement)
 
-bot.run(os.environ.get('TOKEN'))
+@bot.command()
+async def depth(ctx, player, rarity = "Elite", days = 7):
+    player = urllib.parse.quote(player)
+    await ctx.send(
+        f"http://ec2-18-222-189-197.us-east-2.compute.amazonaws.com:8000/depth?play={player}&rarity={rarity}&days={days}")
+
+
+@bot.command()
+async def xray_tier(ctx, user, rarity = 'elite', gt = 'main'):
+    statement = requests.get(f"http://ec2-18-222-189-197.us-east-2.compute.amazonaws.com:8000/xray_tier?user={user}&rarity={rarity}&gt={gt}").json()
+    str_response = json.dumps(statement, indent=2)
+    await ctx.send('```\n' + str_response + '\n```')
+
+  
+@bot.command()
+async def floor_def(ctx, team, rarity_tier):
+    response = requests.get(f"http://ec2-18-222-189-197.us-east-2.compute.amazonaws.com:8000/floor_def?tm={team}&rarity_tier={rarity_tier}").json()
+    str_response = json.dumps(response, indent=2)
+    await ctx.send('```\n' + str_response + '\n```')
+
+
+my_secret = os.environ['TOKEN']
+bot.run(my_secret)
+#bot.run(os.environ.get('TOKEN'))
